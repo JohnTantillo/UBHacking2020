ce5079e6b298ba4845841492fe2408dd9a9e0241
John Tantillo <johntantillo1@gmail.com>
2020-10-24 15:48:45 -0400

    Added multiprocessing for listener and timer to work concurrently

--- src/Hacking/main.py
+++ src/Hacking/main.py
@@ -1,15 +1,23 @@
 from pynput.keyboard import Listener
 import time
+import os
+from multiprocessing import Value
 
-count = 0
+count = Value('i', 0)  # Create a global Value object to track key presses across parent and child
 
 
-def on_press(key):
+def on_press(key):  # Behavior at key press event
     global count
-    count += 1
-    print(count)
+    count.value += 1
+    print(count.value)
 
 
 if __name__ == '__main__':
-    with Listener(on_press=on_press) as listener:
-        listener.join()
+    n = os.fork()  # Fork into parent and child process
+    if n > 0:  # Parent process listens for keyboard events
+        with Listener(on_press=on_press) as listener:
+            listener.join()
+    else:  # Child process waits to update database every minute
+        while True:
+            if time.time() % 60 == 0:
+                print(count.value)
